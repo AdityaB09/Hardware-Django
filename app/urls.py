f226b2213cae603107aa6edbc63281204c3ca088
AdityaB09 <111139561+AdityaB09@users.noreply.github.com>
2022-12-26 11:55:58 +0530

    Password Commit

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+# from django.conf import settings
+# from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from  .forms import MyPasswordChangeForm, MyPasswordResetForm, MySetPasswordForm
 urlpatterns = [
     path('',views.index, name = "index" ),
     path('signup', views.signup, name='signup'),
@@ -8,4 +12,12 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('product/', views.product, name='product'),
     path('product/<slug:data>', views.product, name='productdata'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='passwordchange.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone/' ), name='passwordchange'),
+    path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='passwordchangedone.html'), name='passwordchangedone'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html',form_class=MyPasswordResetForm), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html',form_class=MySetPasswordForm), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Profile, Product 
 from itertools import chain
 from django.core.paginator import Paginator
+
  
 
 # Create your views here.
